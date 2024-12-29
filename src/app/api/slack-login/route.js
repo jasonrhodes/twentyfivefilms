f5fd4123a6459aa2793a7dce71c35a761445a1a6
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2024-12-29 16:58:49 -0500

    Fixes form data parsing

--- src/app/api/slack-login/route.js
+++ src/app/api/slack-login/route.js
@@ -1,7 +1,11 @@
+import * as qs from 'querystring';
+
 export async function POST(request) {
-  const body = await request.json();
+  const bodyText = await request.text();
+  const body = qs.parse(bodyText);
+
   // eslint-disable-next-line no-console
-  console.log(request, body);
+  console.log(JSON.stringify(body));
 
   return Response.json({
     blocks: [
