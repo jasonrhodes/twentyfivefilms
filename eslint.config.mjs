b8b9a07e0fecc3a5b5853bd755ec1ce05b448a3f
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2024-12-29 15:57:48 -0500

    Final lint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
     rules: {
-      'no-console': ["error", { allow: ["warn", "error"] }],
-      'react/prop-types': ["off"]
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'react/prop-types': ['off']
     }
   }
 ];
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
-import Link from "next/link";
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
     <div className="h-[400px] text-2xl flex flex-col justify-between border-red-500">
-        <h1 className="font-black">twenty five films</h1>
-        <p>10 favorites.</p>
-        <p>15 honorable mentions.</p>
-        <p>Choose your twenty five.</p>
-        <p className="border text-center border-white/[0.2] p-4 hover:border-white/[0.4] hover:bg-white/[0.05] cursor-pointer transition-all rounded-md"><Link href="/login">Get Started</Link></p>
+      <h1 className="font-black">twenty five films</h1>
+      <p>10 favorites.</p>
+      <p>15 honorable mentions.</p>
+      <p>Choose your twenty five.</p>
+      <p className="border text-center border-white/[0.2] p-4 hover:border-white/[0.4] hover:bg-white/[0.05] cursor-pointer transition-all rounded-md">
+        <Link href="/login">Get Started</Link>
+      </p>
     </div>
   );
 }
