df61ef7d4c47e7bd063753acf0d4a30a9e943af6
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2024-12-29 17:09:27 -0500

    Updating username from Slack

--- src/app/api/slack-login/route.js
+++ src/app/api/slack-login/route.js
@@ -7,13 +7,15 @@ export async function POST(request) {
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(body));
 
+  const { user_id = 'none_provided' } = body;
+
   return Response.json({
     blocks: [
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: '*Message received, sir!* 🫡'
+          text: `*Message received, <@${user_id}>!* 🫡`
         }
       }
     ]
