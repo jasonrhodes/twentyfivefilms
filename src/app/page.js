78476f8bec69962ef5c9233a3a436e509bd09f0c
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2025-02-11 18:52:57 -0500

    Fixing login styles for rankings page

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,60 @@
+'use client';
+
+import { getSession } from '@/lib/session';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+function Callout({ checkedForLogin, activeSession }) {
+  if (!checkedForLogin) {
+    return null;
+  }
+
+  if (!activeSession) {
+    return (
+      <p>Get a secure login link for your rankings by typing /my25 in Slack</p>
+    );
+  }
+
+  return (
+    <div>
+      <p className="mb-2 text-sm text-center">
+        You're logged in as <b>{activeSession.user.username}</b>
+      </p>
+      <p className="border text-center p-4 border-black/[0.2] hover:border-black/[0.4] hover:bg-black/[0.05] dark:border-white/[0.2] dark:hover:border-white/[0.4] dark:hover:bg-white/[0.05] cursor-pointer transition-all rounded-md">
+        <Link href={`/rankings/${activeSession.user.username}`}>
+          View all of your rankings
+        </Link>
+      </p>
+    </div>
+  );
+}
 
 export default function Home() {
+  const [checkedForLogin, setCheckedForLogin] = useState(false);
+  const [activeSession, setActiveSession] = useState(null);
+
+  useEffect(() => {
+    async function retrieve() {
+      const session = await getSession();
+      if (session) {
+        setActiveSession(session);
+      }
+      setCheckedForLogin(true);
+    }
+
+    retrieve();
+  }, [getSession, setActiveSession]);
+
   return (
     <div className="h-[400px] text-2xl flex flex-col justify-between border-red-500">
       <h1 className="font-black">twenty five films</h1>
       <p>10 favorites.</p>
       <p>15 honorable mentions.</p>
       <p>Choose your twenty five.</p>
-      <p className="border text-center p-4 border-black/[0.2] hover:border-black/[0.4] hover:bg-black/[0.05] dark:border-white/[0.2] dark:hover:border-white/[0.4] dark:hover:bg-white/[0.05] cursor-pointer transition-all rounded-md">
-        <Link href="/rankings">View all of your rankings</Link>
-      </p>
+      <Callout
+        checkedForLogin={checkedForLogin}
+        activeSession={activeSession}
+      />
     </div>
   );
 }
--- src/app/rankings/[username]/page.jsx
+++ src/app/rankings/[username]/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { getSession } from '@/lib/session';
 import { getRankingsForUser } from '@/lib/db';
 import Link from 'next/link';
+import { IconStarFilled } from '@tabler/icons-react';
 
 export default function MyRankings({ params }) {
   const [username, setUsername] = useState(null);
@@ -37,18 +38,41 @@ export default function MyRankings({ params }) {
 
   return (
     <div>
-      <h1>Rankings for {username}</h1>
+      <section className="mb-3">
+        <h1>Your Rankings</h1>
+        <p className="text-sm">
+          You're logged in as <b>{username}</b>
+        </p>
+      </section>
       <ul>
         {rankings.map((r) => (
-          <li key={r.slug}>
-            <strong>
-              <Link href={`/rankings/${username}/${r.slug}`}>{r.name}</Link>
-            </strong>
-            <br />
-            <p>{r.description}</p>
+          <li key={r.slug} className="mb-3">
+            <RankingListItemWrapper slug={r.slug}>
+              <strong className="text-lg">
+                <Link href={`/rankings/${username}/${r.slug}`}>{r.name}</Link>
+              </strong>
+              <br />
+              <p>{r.description}</p>
+            </RankingListItemWrapper>
           </li>
         ))}
       </ul>
     </div>
+  );
+}
+
+function RankingListItemWrapper({ slug, children }) {
+  return (
+    <>
+      <div className="relative">
+        {children}
+        {slug === 'all-time' ? (
+          <>
+            <p>(This is the ranking for the All-Time Favorites Slacket.)</p>
+            <IconStarFilled className="absolute top-0 -left-8 text-yellow-400" />
+          </>
+        ) : null}
+      </div>
+    </>
   );
 }
